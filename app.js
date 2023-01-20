f58e2fbec83429a7175111fdbe6bc14e3a78f6eb
imike964 <imike964@googlemail.com>
2023-01-20 20:33:17 +0000

    JS done

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-function ValidateEmail()
+// const textvar = document.getElementsByClassName("email-prompt");
+
+function ValidateEmail() {
+  let testvar1 = document.querySelector("#email-prompt");
+  let testvar2 = document.querySelector("#icon-error");
+  testvar1.style.display = "flex";
+  testvar1.after.testvar2.style.display = "flex";
+}
+
+document.getElementById("submit").addEventListener("click", ValidateEmail);
