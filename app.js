d9b49e39006ab4978c6ac00bf1140c466331f103
imike964 <imike964@googlemail.com>
2023-01-21 21:41:10 +0000

    done

--- app.js
+++ app.js
@@ -3,8 +3,22 @@
 function ValidateEmail() {
   let testvar1 = document.querySelector("#email-prompt");
   let testvar2 = document.querySelector("#icon-error");
-  testvar1.style.display = "flex";
-  testvar1.after.testvar2.style.display = "flex";
+  let testvar3 =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let testvar4 = document.querySelector("#input1");
+
+  if (testvar4.value.match(testvar3)) {
+    testvar4.value = "";
+    testvar1.style.display = "none";
+    testvar2.style.display = "none";
+  } else {
+    testvar1.style.display = "flex";
+    testvar2.style.display = "flex";
+  }
 }
 
 document.getElementById("submit").addEventListener("click", ValidateEmail);
+
+// let str = "I am not going to be rich"
+// let result = str.replace(/not/g,'');
+// console.log(result);
